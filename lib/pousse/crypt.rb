fb7a0d826118f4d47e24b7a52960cb81d4aede12
Luc Boissaye <luc@boissaye.fr>
2013-06-25 17:07:35 +0200

    Fix inclusion rededuncy. crypt

--- lib/pousse/crypt.rb
+++ lib/pousse/crypt.rb
@@ -1,15 +1,12 @@
-require 'base64'
-require 'digest'
 require 'openssl'
+require 'base64'
+require 'openssl/cipher'
+require 'openssl/digest'
 
 module Pousse
   module Crypt
 
     def self.decrypt(data, key, iv)
-      require 'openssl'
-      require 'base64'
-      require 'openssl/cipher'
-      require 'openssl/digest'
       aes = OpenSSL::Cipher::Cipher.new('aes-256-cbc')
       aes.decrypt
       aes.key = Digest::SHA256.digest(key)
@@ -19,16 +16,11 @@ module Pousse
     end
 
     def self.encrypt(data, key, iv = nil)
-      require 'openssl'
-      require 'base64'
-      require 'openssl/cipher'
-      require 'openssl/digest'
-
       iv ||= Base64.encode64(rand.to_s).gsub("\n",'')
       aes = OpenSSL::Cipher::Cipher.new('aes-256-cbc')
       aes.encrypt
       aes.key = Digest::SHA256.digest(key)
-      aes.iv  = iv 
+      aes.iv  = iv
 
       res = Base64.encode64( aes.update(data) << aes.final ).gsub("\n", '')
       return [res, iv]
